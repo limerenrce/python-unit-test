9b6a8e54e83457ecbc790ad7d1d645bff07b780a
Satya Uttami <satyautta@gmail.com>
2024-12-28 22:15:04 +0800

    unit test merge_sort and quick_sort are success

--- src/utils/test_helpers.py
+++ src/utils/test_helpers.py
@@ -1,6 +1,8 @@
-# This file can include helper methods for testing purposes.
+# Helper methods for testing purposes.
+
 
 def generate_random_array(size, min_val, max_val):
     """Generates an array of random integers for testing."""
     import random
+
     return [random.randint(min_val, max_val) for _ in range(size)]
